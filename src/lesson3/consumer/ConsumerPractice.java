791d3b36313072f75818671e035ee4b384e65bfe
roosvelandia <47333603+roosvelandia@users.noreply.github.com>
2024-05-15 12:12:31 -0500

    add lesson3 Supplier

--- src/lesson3/consumer/ConsumerPractice.java
+++ src/lesson3/consumer/ConsumerPractice.java
@@ -1,2 +1,33 @@
-package lesson3.consumer;public class ConsumerPractice {
+package lesson3.consumer;
+
+import java.util.List;
+import java.util.function.Consumer;
+
+public class ConsumerPractice {
+    public static void main(String[] args) {
+        List<Integer> intList = List.of(34,67,8,23,67,89,90);
+
+        // with integers
+        Consumer<Integer> consumer = i -> System.out.println(i);
+
+        consumer.accept(56);
+
+        printList(intList, consumer);
+
+        // with strings
+
+        List<String> strList = List.of("hello", "i'm", "MELI");
+
+        Consumer<String> consumerString = s -> System.out.println(s);
+
+        consumerString.accept("test");
+
+        printList(strList, consumerString);
+    }
+
+    private static <T> void printList(List<T> intList, Consumer<T> consumer) {
+        for(T i : intList){
+            consumer.accept(i);
+        }
+    }
 }
--- src/lesson3/supplier/SupplierPractice.java
+++ src/lesson3/supplier/SupplierPractice.java
@@ -1,2 +1,14 @@
-package lesson3.supplier;public class SupplierPractice {
+package lesson3.supplier;
+
+import java.util.function.Supplier;
+
+public class SupplierPractice {
+    public static void main(String[] args) {
+        // with string
+        Supplier<String> stringSupplier = () -> "Hello World!";
+        System.out.println(stringSupplier.get());
+        //with lambda
+        Supplier<Double> randmoNumber = () -> Math.random();
+        System.out.println(randmoNumber.get());
+    }
 }
