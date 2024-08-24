40dac2fd2a4c7bcd61b83d90338691fa8ffe02e3
roosvelandia <47333603+roosvelandia@users.noreply.github.com>
2024-08-24 15:51:22 -0500

    Parallel streams pool

--- src/lesson8/Types/Parallel/SettingParallelism.java
+++ src/lesson8/Types/Parallel/SettingParallelism.java
@@ -2,21 +2,28 @@ package lesson8.Types.Parallel;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ForkJoinPool;
 
-public class ParallelStreams {
+public class SettingParallelism {
 
     public static void main(String[] args) {
+        System.out.println("Number of cores: " + Runtime.getRuntime().availableProcessors());
+        System.setProperty("java.util.concurrent.ForkJoinPool.common.parallelism", "5");
+        System.out.println("Parallelism: " + ForkJoinPool.getCommonPoolParallelism());
+
+        ForkJoinPool forkJoinPool = new ForkJoinPool(1);
 
         List<Employee> employees = new ArrayList<>();
 
-        for (int i = 0; i < 10000; i++) {
+        for (long i = 0; i < 100000L; i++) {
             employees.add(new Employee("John", 50000));
             employees.add(new Employee("Sara", 100000));
             employees.add(new Employee("Mike", 80000));
             employees.add(new Employee("Sara", 60000));
             employees.add(new Employee("John", 30000));
         }
-// sequential stream
+
+        // sequential stream
         long startTime = System.currentTimeMillis();
         System.out.println("total employees = " + employees.stream().filter(e -> e.getSalary() > 60000)
                 .count());
@@ -31,5 +38,15 @@ public class ParallelStreams {
         long endParallelTime = System.currentTimeMillis();
 
         System.out.println("Parallel stream took: " + (endParallelTime - startParallelTime) + " ms");
+
+        // with a pool
+        long startParallelThreadTime = System.currentTimeMillis();
+        forkJoinPool.execute(() -> {
+            System.out.println("total employees = " + employees.parallelStream().filter(e -> e.getSalary() > 60000)
+                    .count());
+        });
+        long endParallelThreadTime = System.currentTimeMillis();
+
+        System.out.println("with more threads stream took: " + (endParallelThreadTime - startParallelThreadTime) + " ms");
     }
 }
