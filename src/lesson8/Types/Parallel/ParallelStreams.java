d0040499adf43bb7d9f5e9889f5ad201ef4294b0
roosvelandia <47333603+roosvelandia@users.noreply.github.com>
2024-08-24 15:34:05 -0500

    Parallel streams

---
+++ src/lesson8/Types/Parallel/ParallelStreams.java
@@ -0,0 +1,35 @@
+package lesson8.Types.Parallel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ParallelStreams {
+
+    public static void main(String[] args) {
+
+        List<Employee> employees = new ArrayList<>();
+
+        for (int i = 0; i < 100; i++) {
+            employees.add(new Employee("John", 50000));
+            employees.add(new Employee("Sara", 100000));
+            employees.add(new Employee("Mike", 80000));
+            employees.add(new Employee("Sara", 60000));
+            employees.add(new Employee("John", 30000));
+        }
+// sequential stream
+        long startTime = System.currentTimeMillis();
+        System.out.println("total employees = " + employees.stream().filter(e -> e.getSalary() > 60000)
+                .count());
+        long endTime = System.currentTimeMillis();
+
+        System.out.println("Sequential stream took: " + (endTime - startTime) + " ms");
+
+        // parallel stream
+        long startParallelTime = System.currentTimeMillis();
+        System.out.println("total employees = " + employees.parallelStream().filter(e -> e.getSalary() > 60000)
+                .count());
+        long endParallelTime = System.currentTimeMillis();
+
+        System.out.println("Parallel stream took: " + (endParallelTime - startParallelTime) + " ms");
+    }
+}
